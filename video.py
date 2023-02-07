4566d1fbf2a52a8091629ab7ebcfa931f0490f2a
Gaurav Bhat <42300572+Grbmax@users.noreply.github.com>
2023-02-07 13:42:25 +0530

    change

--- video.py
+++ video.py
@@ -1,26 +1,14 @@
 import cv2
 import pytesseract
 import numpy as np
+import threading
 from pytesseract import Output
 from gtts import gTTS
-from time import sleep
 import os
 import pyglet
 
-cap = cv2.VideoCapture(0)
-
 def get_gray(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-def thresholding(image):
-    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-
-def opening(image):
-    kernel = np.ones((5,5), np.uint8)
-    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
-
-def canny(image):
-    return cv2.Canny(image, 100, 200)
 
 def blur_detect(image):
     return cv2.Laplacian(image, cv2.CV_64F).var()
@@ -31,26 +19,28 @@ def speak(text):
     tts.save(filename)
     music = pyglet.media.load(filename, streaming=False)
     music.play()
-    sleep(music.duration) #prevent from killing
     os.remove(filename)
+
+def play_audio(text):
+    t = threading.Thread(target=speak, args=(text,))
+    t.start()
+
+cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
     gray = get_gray(frame)
     bd = blur_detect(gray)
-    #thresh = thresholding(gray)
-    #opening = opening(gray)
-    #canny = canny(gray)
 
     if bd < 100:
         print("Image too blurry, try again")
-        speak('Image too blurry, try again')
+        play_audio('Image too blurry, try again')
 
     if bd > 100 :
-        t = pytesseract.image_to_string(gray) #You can use other images here which are being processed, eg. opening, canny, etc, based on your requirements.
+        t = pytesseract.image_to_string(gray)
         if t and t.strip() != "":
             print("Grayscale Image: ",t)
-            speak(t)
+            play_audio(t)
         else : 
             print("Nothing Detected in Grayscale Image")
 
