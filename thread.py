f8ee9ec68eb00547ef110c5fdc5ab71261704a34
Gaurav Bhat <42300572+Grbmax@users.noreply.github.com>
2023-02-07 13:54:11 +0530

    init

--- thread.py
+++ thread.py
@@ -1,54 +1,42 @@
 import cv2
 import pytesseract
 import numpy as np
-import multiprocessing as mp
-from gtts import gTTS
-import os
-import pyglet
+from pytesseract import Output
 import threading
+
+def speak(text):
+    print("Speaking: ", text)
 
 def get_gray(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-def speak(text):
-    tts = gTTS(text=text, lang='en')
-    filename = "img_op.mp3"
-    tts.save(filename)
-    music = pyglet.media.load(filename, streaming=False)
-    music.play()
-    os.remove(filename)
+def blur_detect(image):
+    return cv2.Laplacian(image, cv2.CV_64F).var()
 
-def process_image(gray):
+def extract_text_from_image(gray):
     t = pytesseract.image_to_string(gray)
     if t and t.strip() != "":
-        print("Grayscale Image: ",t)
-        speak(t)
-    else: 
-        print("Nothing Detected in Grayscale Image")
+        print("Detected Text: ", t)
+        tts_thread = threading.Thread(target=speak, args=(t,))
+        tts_thread.start()
+    else :
+        print("No text detected in the image")
 
-def run_detection(q):
-    cap = cv2.VideoCapture(0)
-    while True:
-        ret, frame = cap.read()
-        gray = get_gray(frame)
-        q.put(gray)
-        cv2.imshow("Output", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'): #Press q to exit loop
-            break
-    cap.release()
-    cv2.destroyAllWindows()
+cap = cv2.VideoCapture(0)
 
-def run_tts(q):
-    while True:
-        gray = q.get()
-        process_image(gray)
+while True:
+    ret, frame = cap.read()
+    gray = get_gray(frame)
+    bd = blur_detect(gray)
 
-if __name__ == '__main__':
-    q = mp.Queue()
-    t1 = threading.Thread(target=run_detection, args=(q,))
-    t2 = threading.Thread(target=run_tts, args=(q,))
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
+    if bd < 100:
+        print("Image too blurry, try again")
+    else:
+        extract_text_from_image(gray)
 
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
